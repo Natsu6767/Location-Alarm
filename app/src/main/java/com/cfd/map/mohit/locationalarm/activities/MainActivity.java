ff07e71e3ab652161d762576b5c16c309d503e51
Mohit Jain <mohitjain1999@yahoo.com>
2017-02-03 15:43:38 +0530

    Add extra files

--- app/src/main/java/com/cfd/map/mohit/locationalarm/activities/MainActivity.java
+++ app/src/main/java/com/cfd/map/mohit/locationalarm/activities/MainActivity.java
@@ -38,28 +38,27 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GeoAlarmAdapter mAdapter;
-    private ArrayList<GeoAlarm> mAlarms;
     final Context context = MainActivity.this;
     final private int REQUEST_CODE = 1;
     double lati, lang;
     RecyclerView mRecyclerView;
-
     // Location variables
     LocationManager locationManager;
     LocationListener locationListener;
     LatLng alarmPos;
-
+    private GeoAlarmAdapter mAdapter;
+    private ArrayList<GeoAlarm> mAlarms;
+    private double radius = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        alarmPos = new LatLng(0, 0);
+        //alarmPos = new LatLng(0, 0);
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-                if (calculateDis(alarmPos, loc) < 100) {
+                if (calculateDis(alarmPos, loc) < radius) {
                     Toast.makeText(MainActivity.this, "You have arrived", Toast.LENGTH_SHORT).show();
                     locationManager.removeUpdates(locationListener);
                 }
@@ -123,10 +122,100 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        ((GeoAlarmAdapter) mAdapter).setOnItemClickListener(new GeoAlarmAdapter.MyClickListener() {
-            @Override
-            public void onItemClick(final int position, View v) {
-                //On click event for row items
+        ((GeoAlarmAdapter) mAdapter).setOnItemClickListener(
+                new GeoAlarmAdapter.MyClickListener() {
+                    @Override
+                    public void onItemClick(final int position, View v) {
+                        //On click event for row items
+                        //Creates the dialog for configuring the new alarm
+                        LayoutInflater li = LayoutInflater.from(context);
+                        View promptsView = li.inflate(R.layout.location_alarm_dialog, null);
+
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                        // set prompts.xml to alertdialog builder
+                        alertDialogBuilder.setView(promptsView);
+
+                        final EditText userInput = (EditText) promptsView.findViewById(R.id.alarm_name_input);
+                        TextView locationShow = (TextView) promptsView.findViewById(R.id.location_coordinates);
+                        final Spinner ringtoneSelect = (Spinner) promptsView.findViewById(R.id.ringtone);
+                        final CheckBox vibration = (CheckBox) promptsView.findViewById(R.id.vibration);
+                        vibration.setChecked(mAlarms.get(position).getVibration());
+                        locationShow.setText(mAlarms.get(position).getLocationCoordinate());
+                        userInput.setText(mAlarms.get(position).getName());
+
+                        //Use to retreive ringtones from the phone
+                        final Map<String, Ringtone> ringtones = new HashMap<>();
+                        RingtoneManager manager = new RingtoneManager(MainActivity.this);
+                        manager.setType(RingtoneManager.TYPE_ALARM);
+                        Cursor cursor = manager.getCursor();
+                        cursor.moveToFirst();
+                        while (!cursor.isAfterLast()) {
+                            ringtones.put(cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX), manager.getRingtone(cursor.getPosition()));
+                            cursor.moveToNext();
+                        }
+
+                        //Extracts the names of the ringtones
+                        final ArrayList<String> ringtoneNames = new ArrayList<String>();
+                        for (Map.Entry<String, Ringtone> entry : ringtones.entrySet()) {
+                            ringtoneNames.add(entry.getKey());
+                        }
+                        //Puts the values in the ringtone spinner
+                        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_item, ringtoneNames);
+                        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        ringtoneSelect.setAdapter(dataAdapter);
+
+
+                        // set dialog message
+                        alertDialogBuilder
+                                .setCancelable(true)
+                                .setPositiveButton("SAVE",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                //Sets the alarm. Code needs to be entered
+                                                mAlarms.get(position).setName(userInput.getText().toString());
+                                                mAlarms.get(position).setRingtone(ringtoneSelect.getSelectedItem().toString(),
+                                                        ringtones.get(ringtoneSelect.getSelectedItem()));
+                                                mAlarms.get(position).setVibration(vibration.isChecked());
+
+                                                mAdapter.refreshItem(position);
+                                            }
+                                        })
+                                .setNegativeButton("Cancel",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.cancel();
+                                            }
+                                        });
+
+                        // create alert dialog
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+
+                        // show it
+                        alertDialog.show();
+
+                    }
+                }
+
+
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("Intent", "" + resultCode);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE) {
+                // coordinates for destination
+                lati = data.getDoubleExtra("latitude", 0);
+                lang = data.getDoubleExtra("longitude", 0);
+                alarmPos = new LatLng(lati, lang);
+
+                Log.d("location", alarmPos.toString());
+                Toast.makeText(this, "" + lati + ", " + lang, Toast.LENGTH_SHORT).show();
+
                 //Creates the dialog for configuring the new alarm
                 LayoutInflater li = LayoutInflater.from(context);
                 View promptsView = li.inflate(R.layout.location_alarm_dialog, null);
@@ -138,13 +227,11 @@ public class MainActivity extends AppCompatActivity {
 
                 final EditText userInput = (EditText) promptsView.findViewById(R.id.alarm_name_input);
                 TextView locationShow = (TextView) promptsView.findViewById(R.id.location_coordinates);
+//Button locationSet = (Button) promptsView.findViewById(R.id.select_location);
                 final Spinner ringtoneSelect = (Spinner) promptsView.findViewById(R.id.ringtone);
                 final CheckBox vibration = (CheckBox) promptsView.findViewById(R.id.vibration);
-                vibration.setChecked(mAlarms.get(position).getVibration());
-                locationShow.setText(mAlarms.get(position).getLocationCoordinate());
-                userInput.setText(mAlarms.get(position).getName());
 
-                //Use to retreive ringtones from the phone
+//Use to retreive ringtones from the phone
                 final Map<String, Ringtone> ringtones = new HashMap<>();
                 RingtoneManager manager = new RingtoneManager(MainActivity.this);
                 manager.setType(RingtoneManager.TYPE_ALARM);
@@ -154,32 +241,38 @@ public class MainActivity extends AppCompatActivity {
                     ringtones.put(cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX), manager.getRingtone(cursor.getPosition()));
                     cursor.moveToNext();
                 }
+//cursor.close();
 
-                //Extracts the names of the ringtones
+//Extracts the names of the ringtones
                 final ArrayList<String> ringtoneNames = new ArrayList<String>();
                 for (Map.Entry<String, Ringtone> entry : ringtones.entrySet()) {
                     ringtoneNames.add(entry.getKey());
                 }
                 //Puts the values in the ringtone spinner
-                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(MainActivity.this,
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                         android.R.layout.simple_spinner_item, ringtoneNames);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 ringtoneSelect.setAdapter(dataAdapter);
+/**********************************************************************************************************************
+ locationSet.setOnClickListener(new View.OnClickListener() {
+@Override public void onClick(View v) {
+startActivityForResult(new Intent(MainActivity.this, CustomPlacePicker.class), REQUEST_CODE);
+}
 
+});
+ ***********************************************************************************************************************/
 
                 // set dialog message
                 alertDialogBuilder
                         .setCancelable(true)
-                        .setPositiveButton("SAVE",
+                        .setPositiveButton("OK",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int id) {
                                         //Sets the alarm. Code needs to be entered
-                                        mAlarms.get(position).setName(userInput.getText().toString());
-                                        mAlarms.get(position).setRingtone(ringtoneSelect.getSelectedItem().toString(),
-                                                ringtones.get(ringtoneSelect.getSelectedItem()));
-                                        mAlarms.get(position).setVibration(vibration.isChecked());
-
-                                        mAdapter.refreshItem(position);
+                                        setAlarm(userInput.getText().toString(),
+                                                new LocationCoordiante(lati, lang), vibration.isChecked(),
+                                                ringtones.get(ringtoneSelect.getSelectedItem()),
+                                                ringtoneSelect.getSelectedItem().toString());
                                     }
                                 })
                         .setNegativeButton("Cancel",
@@ -195,119 +288,26 @@ public class MainActivity extends AppCompatActivity {
                 // show it
                 alertDialog.show();
 
+                locationShow.setText("" + lati + ", " + lang);
             }
         }
+    }
 
+    // distance formula from current location
+    private double calculateDis(LatLng destiny, LatLng myLoc) {
+        double R = 6371e3; // metres
+        double φ1 = Math.PI * destiny.latitude / 180;
+        double φ2 = Math.PI * myLoc.latitude / 180;
+        double Δφ = φ2 - φ1;
+        double Δλ = Math.PI * (destiny.longitude - myLoc.longitude) / 180;
 
+        double a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-
-);
-        }
-
-@Override
-protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        Log.d("Intent",""+resultCode);
-        if(resultCode==RESULT_OK){
-        if(requestCode==REQUEST_CODE){
-        // coordinates for destination
-        lati=data.getDoubleExtra("latitude",0);
-        lang=data.getDoubleExtra("longitude",0);
-        alarmPos=new LatLng(lati,lang);
-        Log.d("location",alarmPos.toString());
-        Toast.makeText(this,""+lati+", "+lang,Toast.LENGTH_SHORT).show();
-
-        //Creates the dialog for configuring the new alarm
-        LayoutInflater li=LayoutInflater.from(context);
-        View promptsView=li.inflate(R.layout.location_alarm_dialog,null);
-
-        AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(context);
-
-        // set prompts.xml to alertdialog builder
-        alertDialogBuilder.setView(promptsView);
-
-final EditText userInput=(EditText)promptsView.findViewById(R.id.alarm_name_input);
-        TextView locationShow=(TextView)promptsView.findViewById(R.id.location_coordinates);
-//Button locationSet = (Button) promptsView.findViewById(R.id.select_location);
-final Spinner ringtoneSelect=(Spinner)promptsView.findViewById(R.id.ringtone);
-final CheckBox vibration=(CheckBox)promptsView.findViewById(R.id.vibration);
-
-//Use to retreive ringtones from the phone
-final Map<String, Ringtone>ringtones=new HashMap<>();
-        RingtoneManager manager=new RingtoneManager(MainActivity.this);
-        manager.setType(RingtoneManager.TYPE_ALARM);
-        Cursor cursor=manager.getCursor();
-        cursor.moveToFirst();
-        while(!cursor.isAfterLast()){
-        ringtones.put(cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX),manager.getRingtone(cursor.getPosition()));
-        cursor.moveToNext();
-        }
-//cursor.close();
-
-//Extracts the names of the ringtones
-final ArrayList<String>ringtoneNames=new ArrayList<String>();
-        for(Map.Entry<String, Ringtone>entry:ringtones.entrySet()){
-        ringtoneNames.add(entry.getKey());
-        }
-        //Puts the values in the ringtone spinner
-        ArrayAdapter<String>dataAdapter=new ArrayAdapter<String>(this,
-        android.R.layout.simple_spinner_item,ringtoneNames);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ringtoneSelect.setAdapter(dataAdapter);
-/**********************************************************************************************************************
- locationSet.setOnClickListener(new View.OnClickListener() {
-@Override public void onClick(View v) {
-startActivityForResult(new Intent(MainActivity.this, CustomPlacePicker.class), REQUEST_CODE);
-}
-
-});
- ***********************************************************************************************************************/
-
-        // set dialog message
-        alertDialogBuilder
-        .setCancelable(true)
-        .setPositiveButton("OK",
-        new DialogInterface.OnClickListener(){
-public void onClick(DialogInterface dialog,int id){
-        //Sets the alarm. Code needs to be entered
-        setAlarm(userInput.getText().toString(),
-        new LocationCoordiante(lati,lang),vibration.isChecked(),
-        ringtones.get(ringtoneSelect.getSelectedItem()),
-        ringtoneSelect.getSelectedItem().toString());
-        }
-        })
-        .setNegativeButton("Cancel",
-        new DialogInterface.OnClickListener(){
-public void onClick(DialogInterface dialog,int id){
-        dialog.cancel();
-        }
-        });
-
-        // create alert dialog
-        AlertDialog alertDialog=alertDialogBuilder.create();
-
-        // show it
-        alertDialog.show();
-
-        locationShow.setText(""+lati+", "+lang);
-        }
-        }
-        }
-// distance formula from current location
-private double calculateDis(LatLng destiny,LatLng myLoc){
-        double R=6371e3; // metres
-        double φ1=Math.PI*destiny.latitude/180;
-        double φ2=Math.PI*myLoc.latitude/180;
-        double Δφ=φ2-φ1;
-        double Δλ=Math.PI*(destiny.longitude-myLoc.longitude)/180;
-
-        double a=Math.sin(Δφ/2)*Math.sin(Δφ/2)+
-        Math.cos(φ1)*Math.cos(φ2)*
-        Math.sin(Δλ/2)*Math.sin(Δλ/2);
-        double c=2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
-
-        double d=R*c;
+        double d = R * c;
         return d;
-        }
+    }
 
-        }
+}
