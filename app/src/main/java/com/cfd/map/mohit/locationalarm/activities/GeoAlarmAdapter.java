81983e00aef65376787638a280f7a37dbd24bf08
Mohit Jain <mohitjain1999@yahoo.com>
2017-02-03 08:57:41 +0530

    Add to dialog box and custom row of recycler view

--- app/src/main/java/com/cfd/map/mohit/locationalarm/activities/GeoAlarmAdapter.java
+++ app/src/main/java/com/cfd/map/mohit/locationalarm/activities/GeoAlarmAdapter.java
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.cfd.map.mohit.locationalarm.R;
 
@@ -25,12 +27,19 @@ public class GeoAlarmAdapter extends RecyclerView.Adapter<GeoAlarmAdapter.AlarmH
     public static class AlarmHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         //declare view items for each view in alarm_item.xml
+        TextView alarmName, alarmLocation, alarmRingtone;
+        CheckBox alarmVibration;
 
 
         public AlarmHolder(View v) {
             super(v);
 
             //find each view in the alarm_item custom row
+            alarmName = (TextView) v.findViewById(R.id.alarm_name);
+            alarmLocation = (TextView) v.findViewById(R.id.alarm_location);
+            alarmRingtone = (TextView) v.findViewById(R.id.alarm_ringtone);
+            alarmVibration = (CheckBox) v.findViewById(R.id.alarm_vibration);
+
 
             v.setOnClickListener(this);
         }
@@ -59,6 +68,10 @@ public class GeoAlarmAdapter extends RecyclerView.Adapter<GeoAlarmAdapter.AlarmH
     public void onBindViewHolder(GeoAlarmAdapter.AlarmHolder holder, int position) {
 
         //Set values to the alarm_item layout (custom row)
+        holder.alarmName.setText(mAlarms.get(position).getName());
+        holder.alarmLocation.setText(mAlarms.get(position).getLocationCoordinate());
+        holder.alarmRingtone.setText(mAlarms.get(position).getRingtoneName());
+        holder.alarmVibration.setSelected(mAlarms.get(position).getVibration());
 
     }
 
